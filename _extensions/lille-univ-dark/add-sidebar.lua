357ae9dda2262bd9ba462436d643077ce3e591e2
Arthur Bréant <arthur@thinkr.fr>
2023-12-10 04:05:49 +0100

    [fix] Fix the path to the sidebar

--- _extensions/lille-univ-dark/add-sidebar.lua
+++ _extensions/lille-univ-dark/add-sidebar.lua
@@ -1,6 +1,6 @@
 function Header(el)
   if not el.attributes['background-image'] then
-    el.attributes['background-image'] = '_extensions/lille-univ-dark/sidebar.png'
+    el.attributes['background-image'] = '_extensions/ArthurData/lille-univ-dark/sidebar.png'
     el.attributes['background-size'] = '5% 100%'
     el.attributes['background-position'] = 'left'
   end
