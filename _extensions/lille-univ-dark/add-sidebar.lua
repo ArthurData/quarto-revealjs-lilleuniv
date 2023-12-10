84fa0d34f5eda8b29f5ab6ddd69375f6a5d66b45
Arthur Bréant <arthur@thinkr.fr>
2023-12-10 16:42:05 +0100

    [chore] copy files to local folder

--- _extensions/lille-univ-dark/add-sidebar.lua
+++ _extensions/lille-univ-dark/add-sidebar.lua
@@ -1,6 +1,12 @@
+local function copyResource(file)
+  path = quarto.utils.resolvePath(file)
+  quarto.doc.addFormatResource(path)
+end
+
 function Header(el)
+  copyResource('sidebar-dark.png')
   if not el.attributes['background-image'] then
-    el.attributes['background-image'] = '_extensions/ArthurData/lille-univ-dark/sidebar.png'
+    el.attributes['background-image'] = 'sidebar-dark.png'
     el.attributes['background-size'] = '5% 100%'
     el.attributes['background-position'] = 'left'
   end
--- _extensions/lille-univ/add-sidebar.lua
+++ _extensions/lille-univ/add-sidebar.lua
@@ -1,6 +1,12 @@
+local function copyResource(file)
+  path = quarto.utils.resolvePath(file)
+  quarto.doc.addFormatResource(path)
+end
+
 function Header(el)
+  copyResource('sidebar.png')
   if not el.attributes['background-image'] then
-    el.attributes['background-image'] = '_extensions/ArthurData/lille-univ/sidebar.png'
+    el.attributes['background-image'] = 'sidebar.png'
     el.attributes['background-size'] = '5% 100%'
     el.attributes['background-position'] = 'left'
   end
